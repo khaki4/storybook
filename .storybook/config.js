d8af9737d00e302512f5dabe5f0f5b8a99623ad3
pi3rrot <4dakto@gmail.com>
2018-12-07 23:12:29 +0900

    add a welcome page

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,6 +3,7 @@ import { configure } from '@storybook/react';
 const req = require.context('../src', true, /.stories.js$/);
 
 function loadStories() {
+  require('./welcomStory');
   req.keys().forEach(file => req(file));
 }
 
